270bb2f619fbb255b6f697812bf0ed9039a76127
Augusto Leal <aleal4@gmail.com>
2022-02-17 08:03:56 -0800

    galery js

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ function ativarLink(link) {
   const url = location.href;
   const href = link.href;
   if (url.includes(href)) {
-    link.classList.add('ativo')
+    link.classList.add("ativo");
   }
 }
 
@@ -16,35 +16,50 @@ links.forEach(ativarLink);
 
 const parametros = new URLSearchParams(location.search);
 
-function ativarProduto (parametro) {
+function ativarProduto(parametro) {
   const elemento = document.getElementById(parametro);
-  if(elemento) {
+  if (elemento) {
     elemento.checked = true;
   }
 }
 
-parametros.forEach(ativarProduto)
+parametros.forEach(ativarProduto);
 
 // Perguntas Frequentes
 
-const perguntas = document.querySelectorAll('.perguntas button');
+const perguntas = document.querySelectorAll(".perguntas button");
 
-function ativarPergunta (event){
+function ativarPergunta(event) {
   const pergunta = event.currentTarget;
-  const controls = pergunta.getAttribute('aria-controls');
+  const controls = pergunta.getAttribute("aria-controls");
   const resposta = document.getElementById(controls);
 
   resposta.classList.toggle("ativa");
   const ativa = resposta.classList.contains("ativa");
-  pergunta.setAttribute('aria-expanded', ativa);
+  pergunta.setAttribute("aria-expanded", ativa);
 }
 
-function eventosPerguntas (pergunta){
-  pergunta.addEventListener('click', ativarPergunta)
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
 }
 
-perguntas.forEach(eventosPerguntas)
+perguntas.forEach(eventosPerguntas);
 
+// Galeria Bicileta
 
+const galeria = document.querySelectorAll(".bicileta-imagens img");
+const galeriaContainer = document.querySelector(".bicileta-imagens");
 
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
 
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
